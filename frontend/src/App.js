1353181db66c4a37e1f684ca11962aa8a011e43a
AMELIA FATMA MUTIA <ameliafatmam@gmail.com>
2023-05-13 10:29:48 +0700

    test

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">photogram</a>
+      </header>
+      <main>main page</main>
+    </div>
+  );
+}
+
+export default App;
